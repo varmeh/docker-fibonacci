87b979f464e319006d8c46fe914fdfb1248f97e9
Varun Mehta <varun.sagi20@gmail.com>
2018-11-07 12:45:27 +0530

    worker implementation & configuration

---
+++ worker/index.js
@@ -0,0 +1,35 @@
+const keys = require('./keys')
+const redis = require('redis')
+
+const redisClient = redis.createClient({
+	host: keys.redisHost,
+	port: keys.redisPort,
+	retry_strategy: () => 1000
+})
+
+const sub = redisClient.duplicate()
+
+function fib(index) {
+	if (n === 0) {
+		return 0
+	}
+	if (n === 1) {
+		return 1
+	}
+
+	let previous = 0
+	let current = 1
+
+	for (let i = 2; i <= n; i++) {
+		current = previous + current
+		previous = current - previous
+	}
+
+	return current
+}
+
+sub.on('message', (chanel, message) => {
+	redisClient.hset('values', message, fib(parseInt(message)))
+})
+
+sub.subscribe('insert')

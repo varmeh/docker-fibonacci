df2d726bf2eb4643246e00fdda8577f11c6181a9
Varun Mehta <varun.sagi20@gmail.com>
2018-11-07 16:23:10 +0530

    client: implementation & clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Fib from './components/Fib'
+import OtherPage from './components/OtherPage'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<header className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+						<Link to="/">Home</Link>
+						<Link to="/otherpage">Other Page</Link>
+					</header>
+					<div>
+						<Route exact path="/" component={Fib} />
+						<Route path="/otherpage" component={OtherPage} />
+					</div>
+				</div>
+			</Router>
+		)
+	}
 }
 
-export default App;
+export default App

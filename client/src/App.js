ca1db4d1a470931d6d363bff659c446d8b13acba
Varun Mehta <varun.sagi20@gmail.com>
2018-11-12 18:59:00 +0530

    client: version 2 update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-
+import logo from './logo.svg'
+import './App.css'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import Fib from './components/Fib'
 import OtherPage from './components/OtherPage'
+import Fib from './components/Fib'
 
 class App extends Component {
 	render() {
@@ -10,6 +11,8 @@ class App extends Component {
 			<Router>
 				<div className="App">
 					<header className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+						<h1 className="App-title">Fib Calculator version 2</h1>
 						<Link to="/">Home</Link>
 						<Link to="/otherpage">Other Page</Link>
 					</header>
